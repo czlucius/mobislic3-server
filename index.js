1670f4a4b2617399ec3ac936ecba8fde6eac1776
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-02-25 17:14:37 +0800

    File upload functionality

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var exec = require('child_process').exec;
 let express = require('express')
-
+const multer = require("multer");
 
 const app = express()
+const upload = multer({ dest: "uploads/" });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,10 +12,13 @@ app.get("/", (req, res) => {
 	res.send("hello world")
 })
 
-app.post("/3d", (req, res) => {
-
-
+app.post("/3d", upload.single(`file_${new Date().toUTCString()}`),(req, res) => {
+	const file = req.files
+	console.log(file)
 })
+
+
+
 app.listen(28508, ()=>{
 console.log("listening on 28508")
 })
